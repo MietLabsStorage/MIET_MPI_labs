8eed93c6eb8625b85c37ef8bf58c489b155edec6
m_a_ks_ <maksim.m00@mail.ru>
2021-02-14 22:31:25 +0300

    lab1

---
+++ Lab1/Lab1/Source.cpp
@@ -0,0 +1,51 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include "mpi.h"
+#include <string.h>
+
+static void star(int M, char* mes) {
+	int ProcNum, ProcRank;
+	char* RecvRank = new char[255];
+	MPI_Status Status;
+	MPI_Comm_size(MPI_COMM_WORLD, &ProcNum);
+	MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
+
+	for (int im = 0; im < M; im++) {
+		if (ProcRank == 0)
+		{
+			//send messege to any other procs
+			for (int i = 1; i < ProcNum; i++)
+			{
+				MPI_Send(&mes, sizeof(mes), MPI_CHAR, i, 0, MPI_COMM_WORLD);
+				printf("\n%d. Send %s from %d proc to any procs", im, mes, ProcRank);
+			}
+
+			//receive answer from any other procs
+			for (int i = 1; i < ProcNum; i++)
+			{
+				MPI_Recv(&RecvRank, 255, MPI_CHAR, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &Status);
+				printf("\n%d. Receive %s to %d proc from any procs", im, RecvRank, ProcRank);
+			}
+		}
+		else {
+			//receive message from 0 proc
+			MPI_Recv(&RecvRank, 255, MPI_CHAR, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &Status);
+			printf("\n%d. Receive %s to %d proc from 0 proc", im, RecvRank, ProcRank);
+
+			//send answer to 0 proc
+			MPI_Send(&RecvRank, sizeof(RecvRank), MPI_CHAR, 0, 0, MPI_COMM_WORLD);
+			printf("\n%d. Send %s from %d proc to 0 proc", im, mes, ProcRank);
+		}
+	}
+
+}
+
+int main(int argc, char* argv[])
+{
+
+	MPI_Init(&argc, &argv);
+	star(atoi(argv[1]), (char*)"\"it is message\"");
+	MPI_Finalize();
+	return 0;
+}
