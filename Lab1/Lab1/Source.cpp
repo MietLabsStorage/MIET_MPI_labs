32686b8e2bdaceee424db668d2f331dc01b4cac1
m_a_ks_ <maksim.m00@mail.ru>
2021-03-23 11:56:45 +0300

    some redone in lab1

--- Lab1/Lab1/Source.cpp
+++ Lab1/Lab1/Source.cpp
@@ -3,9 +3,9 @@
 #include "mpi.h"
 #include <string>
 
-static void star(int M, char* mes) {
+static void star(int M) {
 	int ProcNum, ProcRank;
-	char* RecvRank = new char[255];
+	int RecvRank;
 	MPI_Status Status;
 	MPI_Comm_size(MPI_COMM_WORLD, &ProcNum);
 	MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
@@ -16,25 +16,26 @@ static void star(int M, char* mes) {
 			//send messege to any other procs
 			for (int i = 1; i < ProcNum; i++)
 			{
-				MPI_Send(&mes, sizeof(mes), MPI_CHAR, i, 0, MPI_COMM_WORLD);
-				printf("\n%d. Send %s from %d proc to any procs", im, mes, ProcRank);
+				//mes += "from 0 proc";
+				MPI_Send(&ProcRank, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+				printf("\n%d. Send \'%d\' from %d proc to any procs", im, ProcRank, ProcRank);
 			}
 
 			//receive answer from any other procs
 			for (int i = 1; i < ProcNum; i++)
 			{
-				MPI_Recv(&RecvRank, 255, MPI_CHAR, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &Status);
-				printf("\n%d. Receive %s to %d proc from any procs", im, RecvRank, ProcRank);
+				MPI_Recv(&RecvRank, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &Status);
+				printf("\n%d. Receive \'%d\' to %d proc from any procs", im, RecvRank, ProcRank);
 			}
 		}
 		else {
 			//receive message from 0 proc
-			MPI_Recv(&RecvRank, 255, MPI_CHAR, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &Status);
-			printf("\n%d. Receive %s to %d proc from 0 proc", im, RecvRank, ProcRank);
+			MPI_Recv(&RecvRank, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &Status);
+			printf("\n%d. Receive \'%d\' to %d proc from 0 proc", im, RecvRank, ProcRank);
 
 			//send answer to 0 proc
-			MPI_Send(&RecvRank, sizeof(mes), MPI_CHAR, 0, 0, MPI_COMM_WORLD);
-			printf("\n%d. Send %s from %d proc to 0 proc", im, mes, ProcRank);
+			MPI_Send(&ProcRank, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+			printf("\n%d. Send \'%d\' from %d proc to 0 proc", im, ProcRank, ProcRank);
 		}
 	}
 
@@ -47,16 +48,8 @@ int main(int argc, char* argv[])
 	int procRank;
 	MPI_Comm_rank(MPI_COMM_WORLD, &procRank);
 
-	int M = 2;
-	char* mes = (char*)"\'this is a message\'";
-		
-	if (procRank == 0)
-	{		
-		star(M, mes);
-	}
-	else {
-		star(M, (char*)"");
-	}
+	int M = 1;
+	star(M);
 
 	
 	MPI_Finalize();
